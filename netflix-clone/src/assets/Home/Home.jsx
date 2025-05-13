f4f1a221f79a1d0a4583390c951f112fb35f5f29
Hiwot32 <demissiehiwot32@gmail.com>
2025-05-13 22:38:19 +0300

    second commit

--- netflix-clone/src/assets/Home/Home.jsx
+++ netflix-clone/src/assets/Home/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Header from '../components/Header/Header.jsx'
+import Footer from '../components/Footer/Footer.jsx'
 
 function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <Header />
+      <Footer />
+      
     </div>
   )
 }
